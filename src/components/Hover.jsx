d1eb8d60d14868d14c6612872e894d63a8a90180
bilcko <iubilcko@gmail.com>
2022-07-14 13:53:41 +0300

    create useHover

--- src/components/Hover.jsx
+++ src/components/Hover.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '../App.css';
+import useHover from "../hooks/useHover";
 
 const Hover = () => {
-    return (
-        <div className='Element' >
+    const ref = useRef()
+    const isHovering = useHover(ref);
 
+    console.log(isHovering)
+    return (
+        <div ref={ref } className={isHovering? 'Element' : ' Element Hover'} >
+            <button onClick={() => { console.log(ref.current)}}>Click me</button>
         </div>
     );
 };
