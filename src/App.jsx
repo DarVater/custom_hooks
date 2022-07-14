a1d19a06de3b9ed56ba7888829cff8ad6dabb7ea
bilcko <iubilcko@gmail.com>
2022-07-14 16:12:34 +0300

    create useRequest

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 import useDebounce from "./hooks/useDebounce";
+import React from "react";
+import axios from "axios";
+import useRequest from "./hooks/useRequest";
 
 function App() {
-    const [value, setValue] = useState('')
-    const debouncedSearch = useDebounce(search, 500)
-    function search(query) {
-        fetch(`https://jsonplaceholder.typicode.com/todos?query=${query}`)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-            })
+    const [todos, loading, error] = useRequest(fetchTodos)
+    function fetchTodos() {
+        return axios.get('https://jsonplaceholder.typicode.com/todos')
     }
 
-    const onChange = e => {
-        setValue(e.target.value)
-        debouncedSearch(e.target.value)
+    if (loading) {
+        return <h1>Идет загрузка...</h1>
+    }
+    if (error) {
+        return <h1>Произошла ошибка при загрузке данных!</h1>
     }
 
     return (
     <div >
-        <input type="text" value={value} onChange={onChange}/>
+        {todos && todos.map(todo =>
+            <div key={todo.id} className='todo-item'>
+                {todo.id}. {todo.title}
+            </div>
+        )}
     </div>
   );
 }
