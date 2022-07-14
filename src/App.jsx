aa2db4a3e417370f973f4e1a3693037fbbac5c32
bilcko <iubilcko@gmail.com>
2022-07-14 13:10:30 +0300

    hook useInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css';
+import useInput from "./hooks/useInput";
 
 function App() {
-  return (
-    <div >
+    const username = useInput('')
+    const password = useInput('')
 
+   return (
+    <div >
+      <input {...username} type="text" placeholder='Username'/>
+      <input {...password} type="password" placeholder='Password'/>
+      <button onClick={() => {console.log(username.value, password.value)}}>Принт в лог</button>
     </div>
   );
 }
